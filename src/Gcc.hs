2611a7f054874700444205682f4d2966950ca912
Julian K. Arni <jkarni@gmail.com>
2014-07-25 16:03:19 +0200

    First post!

---
+++ src/Gcc.hs
@@ -0,0 +1,46 @@
+module Gcc where
+
+import Control.Monad.Free
+
+data GccInstruction cont
+        = LDC Int cont
+        | LD Int Int cont
+        | ADD cont
+        | SUB cont
+        | MUL cont
+        | DIV cont
+        | CEQ cont
+        | CGT cont
+        | CGTE cont
+        | ATOM cont
+        | CONS cont
+        | CAR cont
+        | CDR cont
+        | SEL cont
+        | JOIN cont
+        | LDF Int cont
+        | AP Int cont
+        | RTN cont
+        | DUM Int cont
+        | RAP Int cont
+        | STOP
+        deriving Show
+
+type GccProgram = Free GccInstruction
+data DataStack
+data ControlStack
+data EnvironmentFrame
+data DataHeap
+
+
+
+data GccProgState = GPS { ds :: DataStack
+                        , cs :: ControlStack
+                        , ef :: EnvironmentFrame
+                        , dh :: DataHeap
+                        }
+
+
+interpret :: GccProgram ->  (GccProgState -> GccProgState)
+interpret = undefined
+
