33c73be67867a3e8ecea219db13ec6206c66b7ee
Matthias Fischmann <mf@zerobuzz.net>
2014-07-25 19:37:37 +0200

    scoring function

--- src/Game.hs
+++ src/Game.hs
@@ -1,5 +1,7 @@
 module Game where
 
+import Control.Monad
+
 data Cell = Wall
           | Empty
           | Pill
@@ -7,16 +9,27 @@ data Cell = Wall
           | Fruit
           | LambdaMan
           | Ghost
-          deriving Enum
+          deriving (Enum, Eq)
 
 instance Show Cell where
-        show Wall = "#"
-        show Empty = " "
-        show Pill = "."
-        show PowerPill = "o"
-        show Fruit = "%"
-        show LambdaMan = "\\"
-        show Ghost = "="
+    show c = [cellToChar c]
+
+charToCell '#' = Wall
+charToCell ' ' = Empty
+charToCell '.' = Pill
+charToCell 'o' = PowerPill
+charToCell '%' = Fruit
+charToCell '\\' = LambdaMan
+charToCell '=' = Ghost
+
+cellToChar Wall = '#'
+cellToChar Empty = ' '
+cellToChar Pill = '.'
+cellToChar PowerPill = 'o'
+cellToChar Fruit = '%'
+cellToChar LambdaMan = '\\'
+cellToChar Ghost = '='
+
 
 data GameState = GameState Tick Map Points
 
@@ -42,7 +55,7 @@ run timeout game = _
 
 
 level1 :: Map
-level1 =
+level1 = map (map charToCell) $
     "#######################" :
     "#..........#..........#" :
     "#.###.####.#.####.###.#" :
@@ -70,3 +83,44 @@ level1 =
 
 {-display :: Game -> IO ()-}
 {-display -}
+
+
+
+type Level = Int
+
+score :: Int -> [Cell] -> Map -> Int -> Int -> Int
+score previousScore eatenThings gameMap ghostsEatenNow remainingLives = newScore * sweepBonus
+  where
+    newScore = (10 * length (filter (== Pill) eatenThings))
+             + (50 * length (filter (== PowerPill) eatenThings))
+             + (fruitValue * length (filter (== Fruit) eatenThings))
+             + (frightModeBonus * length (filter (== Ghost) eatenThings))
+
+    fruitValue = case mapLevel gameMap of
+        1  ->  100
+        2  ->  300
+        3  ->  500
+        4  ->  500
+        5  ->  700
+        6  ->  700
+        7  -> 1000
+        8  -> 1000
+        9  -> 2000
+        10 -> 2000
+        11 -> 3000
+        12 -> 3000
+        _  -> 5000
+
+    frightModeBonus = 200 * 2 ^ (min 3 ghostsEatenNow)
+
+    sweepBonus = if null . filter (`elem` [Pill]) . join $ gameMap
+                   then remainingLives + 1
+                   else 1
+                 -- FIXME: Pills, or also PowerPills?
+
+mapLevel :: Map -> Level
+mapLevel m = head [ level | level <- [1..]
+                          , 100 * (level - 1) < mapWidth * mapHeight && mapWidth * mapHeight <= 100 * level ]
+  where
+    mapWidth = length (head m)
+    mapHeight = length m
