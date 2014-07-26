24feef3b1758b3b7608df9aec6f87c71d3e6bece
Matthias Fischmann <mf@zerobuzz.net>
2014-07-26 16:25:48 +0200

    lambdaman can now look at the map!

--- src/LambdaMan.hs
+++ src/LambdaMan.hs
@@ -18,14 +18,18 @@ lm1 = do
     rtn
 
     label "process_world"
+    -- ld 0 0  -- initial game state
+    -- ld 0 1  -- ghost code
+
+    ld 0 0  -- initial game state
+    car  -- map
+    ldf "dump_map"
+    ap 1
     rtn
 
     label "step_func"
     -- ld 0 0  -- ai state
-    ld 0 1  -- game state
-    car     -- map
-    ldf "dump_map"
-    ap 1
+    -- ld 0 1  -- game state
 
     ldc 0  -- state
     ldc 1  -- move right
@@ -34,58 +38,63 @@ lm1 = do
 
     -- (leaves data stack intact.)
     label "dump_map"
+    -- ld 0 0 -- map
+
     ld 0 0
+    ldc 1
+    ldc 0
+    ldf "obj_at_pos"
+    ap 3
     dbug
+
     ld 0 0
-    car
+    ldc 1
+    ldc 1
+    ldf "obj_at_pos"
+    ap 3
     dbug
+
     ld 0 0
+    ldc 1
+    ldc 2
+    ldf "obj_at_pos"
+    ap 3
+    dbug
     rtn
 
+    -- macros
+    obj_at_pos
 
-    -- not working yet: i have to decrement the y / x coordinates in
-    -- the frame in accordance with cdr-ing through the rows / cells.
-    -- this cannot be done on the stack directly, and frames can only
-    -- be written to by writing more function calls.  this is quite
-    -- tedious!
 
+obj_at_pos :: GccProgram String ()
+obj_at_pos = do
     label "obj_at_pos"
     -- ld 0 0  -- map
     -- ld 0 1  -- x
     -- ld 0 2  -- y
 
-    -- load map, y coord, 0 onto stack, and conditionally jump.
     ld 0 0
     ld 0 2
-    ldc 0
-    sel "obj_at_pos__1" "obj_at_pos__2"
+    ldf "obj_at_pos_find_elem"
+    ap 2
+    ld 0 1
+    ldf "obj_at_pos_find_elem"
+    ap 2
     rtn
 
-    -- y == 0: keep current first line on stack; jump to x search.
-    label "obj_at_pos__1"
+    label "obj_at_pos_find_elem"
+    ld 0 0  -- remaining elems
+    ld 0 1  -- index
+    sel "obj_at_pos_find_elem_1" "obj_at_pos_find_elem_0"
+    rtn
+    label "obj_at_pos_find_elem_0"  -- yes-zero
     car
-    ld 0 1
-    ldc 0
-    sel "obj_at_pos__3" "obj_at_pos__4"
-    join
-
-    -- y /= 0: move to next line, repeat.
-    label "obj_at_pos__2"
-    cdr
-    ld 0 2
-    ldc 0
-    sel "obj_at_pos__1" "obj_at_pos__2"
-    join
-
-    -- x == 0: keep current first cell on stack; done!
-    label "obj_at_pos__3"
-    car
-    join
-
-    -- x /= 0: move to next cell, repeat.
-    label "obj_at_pos__4"
+    join_
+    label "obj_at_pos_find_elem_1"  -- non-zero
     cdr
     ld 0 1
-    ldc 0
-    sel "obj_at_pos__3" "obj_at_pos__4"
-    join
+    ldc 1
+    sub
+    ldf "obj_at_pos_find_elem"
+    ap 2
+    join_
