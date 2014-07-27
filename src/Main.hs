51f26901d66f39d62d007be2fc8488a1c3129369
Matthias Fischmann <mf@zerobuzz.net>
2014-07-27 08:20:03 +0200

    main function

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,31 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Main where
 
-import Gcc
+import Data.List
+import Data.String.Conversions
+import System.Directory
+import System.FilePath
+import Text.Show.Pretty
+
+import qualified Data.ByteString.Char8 as SBS
+
 import Game
+import Gcc
 import GccExercises
 import SECD
 
-main = undefined
+
+main :: IO ()
+main = do
+    setCurrentDirectory "../scheme"
+    getDirectoryContents "." >>= mapM_ demoScmToGcc . sort . filter ((== ".scm") . takeExtension)
+
+
+demoScmToGcc :: FilePath -> IO ()
+demoScmToGcc file = do
+    putStrLn $ "\n\n*** " ++ file ++ " ***\n\n"
+    scheme_sample :: SBS <- SBS.readFile file
+    SBS.putStrLn scheme_sample
+    schemeToSECD scheme_sample >>= putStrLn . Data.String.Conversions.cs
+    schemeToGcc scheme_sample >>= \ (Right prog) -> putStrLn . codeGen $ prog
